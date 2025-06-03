b56a2aea69c3ebebe4e03899e9a5c2c1bdf4a06c
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2025-06-04 02:33:26 +0530

    Create ChatbotResponse.js

---
+++ ChatbotResponse.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const chatbotResponseSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    age: { type: String, required: true },
+    zipcode: { type: String, required: true },
+    email: { type: String, required: true },
+    medicare: { type: String, required: true },
+    healthConditions: { type: String, required: true },
+    housingStatus: { type: String, required: true },
+    drivesWeekly: { type: String, required: true },
+    recentDUI: { type: String, required: true },
+    accidents: { type: String, required: true },
+    hasChildren: { type: String, required: true },
+    creditCardDebt: { type: String, required: true },
+    exercises: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ChatbotResponse = mongoose.model("ChatbotResponse", chatbotResponseSchema);
+module.exports = ChatbotResponse;
