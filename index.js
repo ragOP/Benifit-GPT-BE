a3ac8927ba361bc4341e303ada1aaa5fef833440
ragOP <najmiraghib@gmail.com>
2025-08-05 19:45:16 +0530

    fuck u maderchod

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-import Stripe from "stripe";
+const Stripe = require("stripe");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
